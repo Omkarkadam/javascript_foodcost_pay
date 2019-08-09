d42e136542bcfc70f45401c92bdc71191d5a8b94
Omkar Kadam <ryderomkar@yahoo.com>
2019-08-09 10:09:52 +0530

    Update paybills.js

--- paybills.js
+++ paybills.js
@@ -1,3 +1,6 @@
-var foodcost=prompt("");
+var foodcost=prompt("Enter Food cost to be paid:");
+let people=prompt("Enter number of people:")
 document.writeln("The total foodcost was: ",foodcost,"<br>"); 
-document.writeln("Each one has to pay: ", foodcost/8);
+document.writeln("Each one has to pay: ", (foodcost/people).toFixed(2));
+
+
